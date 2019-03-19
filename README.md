31a66bad91fbdf852ac77a4feaf45cc517e041aa
Rohit Suri <rohitsuri.nitr@gmail.com>
2019-03-19 15:48:15 +0100

    More documentation never hurt anyone

---
+++ README.md
@@ -0,0 +1,8 @@
+# Useful scripts
+
+Here's a collection of scripts to help with the Autolab in general and for easy logging
+
+## ssh_without_pass.sh
+
+Use this script to set up automatic login without typing the password during ssh  
+_Requirements:_ install sshpass (`sudo apt-get install sshpass`)
--- ssh_without_pass.sh
+++ ssh_without_pass.sh
@@ -3,16 +3,18 @@
 # You need to install sshpass before using this script
 
 # Set the devices here
-devices=(demowatchtower01 demowatchtower02 demowatchtower03 demowatchtower05 demowatchtower06 demowatchtower07 demowatchtower08 demowatchtower09 demowatchtower10 demowatchtower11 demowatchtower12 demowatchtower13 demowatchtower14 demowatchtower15)
+devices=(demowatchtower01 demowatchtower02 demowatchtower03 demowatchtower05 demowatchtower06 demowatchtower08 demowatchtower09 demowatchtower10 demowatchtower11 demowatchtower12 demowatchtower13 demowatchtower14 demowatchtower15)
 
 # Step 1: Generate a new key if it doesn't exist already
 cat /dev/zero | ssh-keygen -q -N "" >> /dev/null
 
+# Step 2: Iterate through devices above 
 for idx in ${!devices[*]}
 do
 
     printf "\nSetting up %s\n" ${devices[$idx]}
     
+    # Step 3: Do magic
     # This command appends the contents of id_rsa.pub from your computer to authorized_keys on the device. 
     # sshpass makes passing the password easier. 
     # -o StrictHostKeyChecking=no adds the hosts to known_hosts file directly thus skipping the part where you need to type "yes" to ssh into a device for the first time
