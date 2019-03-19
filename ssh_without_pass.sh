e9b598ae00e4273b7d4aac1d3a217b47cca9c357
Rohit Suri <rohitsuri.nitr@gmail.com>
2019-03-19 15:08:49 +0100

    Script to automate process of adding id_rsa.pub to authorized_keys

---
+++ ssh_without_pass.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# You need to install sshpass before using this script
+
+# Set the devices here
+devices=(demowatchtower01 demowatchtower02 demowatchtower03 demowatchtower05 demowatchtower06 demowatchtower07 demowatchtower08 demowatchtower09 demowatchtower10 demowatchtower11 demowatchtower12 demowatchtower13 demowatchtower14 demowatchtower15)
+
+# Step 1: Generate a new key if it doesn't exist already
+cat /dev/zero | ssh-keygen -q -N "" >> /dev/null
+
+for idx in ${!devices[*]}
+do
+
+    printf "\nSetting up %s\n" ${devices[$idx]}
+    
+    # This command appends the contents of id_rsa.pub from your computer to authorized_keys on the device. 
+    # sshpass makes passing the password easier. 
+    # -o StrictHostKeyChecking=no adds the hosts to known_hosts file directly thus skipping the part where you need to type "yes" to ssh into a device for the first time
+    # The rest is straightforward
+    cat ~/.ssh/id_rsa.pub | sshpass -p "MomWatches" ssh -o StrictHostKeyChecking=no mom@${devices[$idx]}.local 'cat >> .ssh/authorized_keys'
+    
+    printf "Done!\n"
+done
